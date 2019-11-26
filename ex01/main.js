a69a9195328aed80f85af3b924e53dbee14ca42b
nssuperx <mgtmgtx@gmail.com>
2019-11-27 00:26:40 +0900

    add gitignore

---
+++ ex01/main.js
@@ -0,0 +1,9 @@
+function sayHallo(){
+    var classElements = document.getElementsByClassName('test');
+    classElements[0].innerHTML = "Hallo world!!!!!"
+}
+
+function eraseHallo(){
+    var classElements = document.getElementsByClassName('test');
+    classElements[0].innerHTML = ""
+}
