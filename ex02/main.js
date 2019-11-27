7ada083a09851899fee2084b82a6fa56607e53b8
nssuperx <mgtmgtx@gmail.com>
2019-11-27 10:26:55 +0900

    make ex02 dir

---
+++ ex02/main.js
@@ -0,0 +1,9 @@
+function sayHello(){
+    var classElements = document.getElementsByClassName('test');
+    classElements[0].innerHTML = "Hello world!!!!!";
+}
+
+function eraseHello(){
+    var classElements = document.getElementsByClassName('test');
+    classElements[0].innerHTML = "";
+}
