693e7b0063ba8c0ba30ed1b0c52f7d4bf689cf4e
nssuperx <mgtmgtx@gmail.com>
2019-11-27 12:50:16 +0900

    2日目完成

--- ex02/main.js
+++ ex02/main.js
@@ -1,9 +1,21 @@
-function sayHello(){
-    var classElements = document.getElementsByClassName('test');
-    classElements[0].innerHTML = "Hello world!!!!!";
+var helloCount = 0;
+function addHello(){
+    helloCount += 1;
+    drawHello();
 }
 
 function eraseHello(){
+    helloCount -= 1;
+    if(helloCount < 0){
+        helloCount = 0;
+    }
+    drawHello();
+}
+
+function drawHello(){
     var classElements = document.getElementsByClassName('test');
     classElements[0].innerHTML = "";
+    for(var i = 0; i < helloCount; i++){
+        classElements[0].innerHTML += "Hello world!!!!!!<br>";
+    }
 }
