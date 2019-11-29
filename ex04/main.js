5874b9d8a410144c59d342a2811425d781d03930
nssuperx <mgtmgtx@gmail.com>
2019-11-29 21:34:43 +0900

    4日目途中

--- ex04/main.js
+++ ex04/main.js
@@ -1,29 +1,23 @@
-window.onload = function(){
-    console.log('onloadした');
-    let classElements = document.getElementsByClassName('test');
-    classElements[0].innerHTML = "";
+function changeColorClass1(){
+
 }
 
-function addString(){
-    let inputtext = document.getElementsByClassName('inputtext')[0].value;
-    console.log(inputtext);
-    if(inputtext.indexOf("<br>") != -1){
-        inputtext = "&lt;br&gt;は入力しないで...";
-    }
-    inputtext += "<br>";
-    let classElements = document.getElementsByClassName('test');
-    classElements[0].innerHTML += inputtext;
+function changeColorClass2(){
+    
 }
 
-function eraseString(){
-    let classElements = document.getElementsByClassName('test');
-    let editString = classElements[0].innerHTML;
-    editString = editString.substring(0,editString.length - 4);
-    let editString_lastidx = editString.lastIndexOf("<br>");
-    if(editString_lastidx == -1){
-        editString = "";
-    }else{
-        editString = editString.substring(0,editString_lastidx + 4);
-    }
-    classElements[0].innerHTML = editString;
+function changeColorClass3(){
+    
+}
+
+function changeColorWhiteClass1(){
+
+}
+
+function changeColorWhiteClass2(){
+    
+}
+
+function changeColorWhiteClass3(){
+    
 }
