94cf38fed756114ddf669cbf78ea1c2433953817
nssuperx <mgtmgtx@gmail.com>
2019-12-04 22:31:57 +0900

    9日目完成

--- ex09/main.js
+++ ex09/main.js
@@ -1,6 +1,7 @@
 window.onload = function(){
     var say = document.getElementById('sayButton');
     var erase = document.getElementById('eraseButton');
+    //ページ読み込み時に、イベントが発生したときにどの関数を呼び出すかを設定している。
     say.addEventListener('click', sayHello, false);
     erase.addEventListener('click', eraseHello, false);
 }
