1daa4058ccf2a4270ad17282c39e8ec8df3db7f0
nssuperx <mgtmgtx@gmail.com>
2019-12-04 21:38:39 +0900

    1日目のにコメントつけた

--- ex01/main.js
+++ ex01/main.js
@@ -1,3 +1,9 @@
+/********************
+ * document.getElementsByClassName()
+ * 指定したクラス名を持った要素(element)をHTMLCollectionという配列みたいなのにまとめて持ってこれる。
+ * 配列みたいにインデックス指定して使える。
+ ********************/
+
 function sayHello(){
     var classElements = document.getElementsByClassName('test');
     classElements[0].innerHTML = "Hello world!!!!!";
