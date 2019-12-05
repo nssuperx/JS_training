5f1949e8ae4a6bb4a5c2ea6903bf6a576130ac7e
nssuperx <mgtmgtx@gmail.com>
2019-12-05 17:39:57 +0900

    10回目途中

--- ex10/main.js
+++ ex10/main.js
@@ -1,21 +1,18 @@
 function addHello(){
-    var parent = document.getElementById('parent');
-    var hello = document.createElement('div');
-    var helloNum = parent.getElementsByClassName('hello').length;
-    hello.className = "hello";
-    hello.innerHTML = helloNum.toString() + "個目の子要素hello";
-    parent.appendChild(hello);
+    var parent = document.getElementById('parent');     //親要素を取得
+    var hello = document.createElement('div');          //divタグの子要素を作成
+    var helloNum = parent.getElementsByClassName('hello').length;       //親要素の中にある指定したクラス名を持つ子要素の数をとってくる
+    hello.className = "hello";                          //クラス名を指定
+    hello.innerHTML = helloNum.toString() + "個目の子要素hello";         //文字列を代入
+    parent.appendChild(hello);                          //親要素の一番後ろに新しく作った子要素を追加
 }
 
 function eraseHello(){
-    var parent = document.getElementById('parent');
-    var helloElements = parent.getElementsByClassName('hello');
+    var parent = document.getElementById('parent');     //親要素を取得
+    parent.removeChild(parent.lastElementChild);        //指定した子要素を削除，ここでは一番後ろの子要素
 }
 
-function drawHello(){
-    var classElements = document.getElementsByClassName('test');
-    classElements[0].innerHTML = "";
-    for(var i = 0; i < helloCount; i++){
-        classElements[0].innerHTML += "Hello world!!!!!!<br>";
-    }
+function changeHello(){
+    var parent = document.getElementById('parent');     //親要素を取得
+
 }
